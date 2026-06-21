4d465538db0b1d0e52791972df3766191e99e8bb
stkggg1220-source <stkggg1220@gmail.com>
2026-06-22 00:30:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const DATA_URL = "/.data.json";
+const DATA_URL = "/data.json";
 
 const teamsWrapper = document.getElementById('teams-wrapper');
 const poolContainer = document.getElementById('pool-container');
